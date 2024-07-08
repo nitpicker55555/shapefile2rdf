72bbe3d16534c8342f4ebd26bb810f62be542b3e
nitpicker55555 <1303385763@qq.com>
2024-07-08 18:25:53 +0200

    move boundingbox into session

--- flask_pro/test5.py
+++ flask_pro/test5.py
@@ -1,33 +1,104 @@
-from ask_functions_agent import *
-# aa=name_cosin('water')
-def data_intersection(query):
-    match_list = name_cosin_list(query)
-    table_name_dicts={}
-    table_fclass_dicts={}
-    all_id_list=[]
-    if len(match_list) != 0:
-        print('name match')
-        table_name_dicts = find_keys_by_values(name_dict_4_similarity, match_list)
-        print(table_name_dicts)
+# -*- coding: utf-8 -*-
 
-    match_list = fclass_cosin_list(query)
-    if len(match_list) != 0:
-        print('fclass match')
-        table_fclass_dicts = find_keys_by_values(fclass_dict_4_similarity, match_list)
-        print(table_fclass_dicts)
-    if table_name_dicts!={} and table_fclass_dicts!={}:
-        table_fclass_dicts.update({'buildings':'building'})
-    for table_ ,fclass_list in table_fclass_dicts.items():
-        if table_ in table_name_dicts:
-            each_id_list = ids_of_type(table_, {'non_area_col': {'fclass': set(fclass_list), 'name': set(table_name_dicts[table_])},'area_num': None})
-            all_id_list.append(each_id_list)
-    return merge_dicts(all_id_list)
-calculation_query('isar river')
-    # for table_, fclass_list in table_fclass_dicts.items():
-    #     each_id_list = ids_of_type(table_, {'non_area_col': {'fclass': set(fclass_list), 'name': set()},
-    #                                         'area_num': None})
+import traceback
+from flask import Flask, Response, stream_with_context, request, render_template, jsonify, session, redirect, url_for
+
+import time
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+import io
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI()
+import sys
+from io import StringIO
+
+from flask_socketio import SocketIO, emit
+output = StringIO()
+original_stdout = sys.stdout
+app = Flask(__name__)
+
+app.secret_key = 'secret_key'  # 用于启用 flash() 方法发送消息
+@app.before_request
+def initialize_session():
+    if 'globals_dict' not in session:
+        session['globals_dict']={'bounding_box_region_name': 'FREISING',
+                              'bounding_coordinates': [48.061625, 48.248098, 11.360777, 11.72291],
+                              'bounding_wkb': '01030000000100000005000000494C50C3B7B82640D9CEF753E3074840494C50C3B7B82640FC19DEACC11F484019E76F4221722740FC19DEACC11F484019E76F4221722740D9CEF753E3074840494C50C3B7B82640D9CEF753E3074840'}
+
+        print('session 初始化')
+
+# global_variables = {}
+
+# 示例的 Markdown 文本（包含图片链接）
+# ![示例图片](/static/data.png "这是一个示例图片")
+socketio = SocketIO(app, manage_session=True, async_mode='threading')
 
 
-# {'buildings': ['isarCenter', 'Isarburg'], 'land': ['isarCenter', 'Isar', 'Isarauenpark', 'Isarstraße', 'Isarburg', 'Isarstausee'], 'points': ['Isar Quelle', 'Isar', 'Isarweg', 'Die Isarauen', 'Isarhotel', 'Isar Durscht', 'Isarstraße', 'Isar Renaturierung'], 'lines': ['Isarleitenweg', 'Isartrail', 'Isarwanderweg', 'Isar', 'Isarlandstraße', 'Isarweg', 'Isarbrücke', 'Isarauenweg', 'Kleine Isar', 'Isarwerkkanal', 'Isarauen', 'Isarauenstraße', 'Isarleiten', 'Isarstraße']}
 
-# {'land': ['riverbank', 'water', 'reservoir'], 'lines': ['river', 'canal', 'stream']}
+@app.route('/')
+def home():
+
+    return render_template('index.html')
+
+
+
+@app.route('/debug_mode', methods=['POST'])
+def debug_mode():
+    data = request.get_json().get('message')  # 获取JSON数据
+    if data == 'debug':
+        session['template'] = True
+    else:
+        session['template'] = False
+    return jsonify({"text": True}), 400
+
+
+
+def send_data(data, mode="data", index="", sid=''):
+    target_labels = []
+
+    if sid != '':
+        socketio.emit('text', {mode: data, 'index': index,'target_label':target_labels}, room=sid)
+    else:
+        print('no sid')
+
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    data = request.get_json().get('text')  # 获取代码
+
+    def generate(data):
+        yield data
+        original_stdout = sys.stdout
+        output = io.StringIO()
+        sys.stdout = output
+
+        try:
+            # Pass the session into the exec environment
+            exec(data, {'session': session, **globals()})
+        except Exception as e:
+            exc_info = traceback.format_exc()
+            if session.get('template') == True:
+                print(f"An error occurred: {repr(e)}\n{exc_info}")
+            else:
+                print("Nothing can I get! Please change an area and search again :)")
+        finally:
+            sys.stdout = original_stdout
+
+        code_result = str(output.getvalue().replace('\00', ''))
+        output.truncate(0)
+        output.seek(0)
+        # print(session['globals_dict'])
+        yield code_result
+
+    # Collect the results from the generator
+    results = ''.join(list(generate(data)))
+    return Response(stream_with_context(generate(data)))
+
+
+
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host='0.0.0.0', port=9090)
